80281ebf4a40f7da605e0740ea24f6a44dfa243d
neven kusturica <neven.kusturica@gmail.com>
2022-04-06 19:05:36 +0200

    Added ex01

---
+++ ex01/src/index.js
@@ -0,0 +1,23 @@
+
+var myPet = {
+    species: "bichon",
+    name: "Mona",
+    legs: 4,
+    friends: "Rex " + " Mimi",
+}
+
+
+function myFunction(myObj){
+    return myObj;
+}    
+
+console.log(myFunction(myPet));
+module.exports = {myPet, myFunction};
+
+
+
+
+
+
+
+
